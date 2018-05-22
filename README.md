34f8f5e7fe78e40a4ee57dea639e3404d41e8622
unknown <@309-11.EU.NET>
2018-05-22 10:12:25 +0300

    spam

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Spam-and-ham
+1. Read initial data from CSV
+2. Text => words 
+    - split
+    - lowercase
+    - clean up special symbols
+    -
+3. Word status
+4. Use Bayes f. detect if text is spam
--- spam.js
+++ spam.js
@@ -1,7 +1,18 @@
 let fs = require('fs');
+
+let getCategory = (line) => line.startsWith('ham') ? 'ham' : 'spam';
+
 fs.readFileSync('spam.csv')
     .toString()
     .split(/\r?\n/)
     .forEach(function(line){
-        console.log(line);
+        let category = getCategory(line);
+        console.log(category);
+
+        line = line.substring(category.length + 1)
+        console.log(line.split(" "));
     });
+
+
+//let parseLine = (line) => ({ category: 'spam', text: ''}) //substring
+//console.log(anyString.substring(5, 0));
